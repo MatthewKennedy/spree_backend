c3608117ddff30a227038a932767b85e01bb37c4
Matthew Kennedy <m.kennedy@me.com>
2022-05-25 00:30:33 +0100

    Move flatpickr to stimulus.

--- app/javascript/spree/dashboard/controllers/flatpickr_controller.js
+++ app/javascript/spree/dashboard/controllers/flatpickr_controller.js
@@ -1,38 +1,29 @@
 import Flatpickr from 'stimulus-flatpickr'
+import { Locales } from '../i18n/flatpickr'
 
-// import the translation files and create a translation mapping
-import { French } from 'flatpickr/dist/l10n/fr.js'
-import { english } from 'flatpickr/dist/l10n/default.js'
-
-// create a new Stimulus controller by extending stimulus-flatpickr wrapper controller
 export default class extends Flatpickr {
-  locales = {
-    fr: French,
-    en: english
-  }
+  locales = Locales
 
   connect () {
-    // define locale and global flatpickr settings for all date pickers
     this.config = {
-      locale: this.locale,
+      locale: SpreeDash.translations.flatpickr_locale,
       altInput: true,
-      showMonths: 2
+      time_24hr: true,
+      altInputClass: 'flatpickr-alt-input form-control rounded-start',
+      showMonths: 1
     }
-
     super.connect()
   }
 
-  // automatically submit form when a date is selected
   change (selectedDates, dateStr, instance) {
-    const form = this.element.closest('form')
-    Rails.fire(form, 'submit')
-  }
+    if (this.element.classList.contains('datePickerFrom')) {
+      const toCal = document.getElementById(this.element.dataset.targetPairId)
+      toCal._flatpickr.set('minDate', selectedDates[0])
+    }
 
-  get locale () {
-    if (this.locales && this.data.has('locale')) {
-      return this.locales[this.data.get('locale')]
-    } else {
-      return ''
+    if (this.element.classList.contains('datePickerTo')) {
+      const fromCal = document.getElementById(this.element.dataset.targetPairId)
+      fromCal._flatpickr.set('maxDate', selectedDates[0])
     }
   }
 }
