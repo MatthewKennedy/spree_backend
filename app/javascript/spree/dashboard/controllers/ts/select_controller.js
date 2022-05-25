f2f2df388cc3f0a5c10de3dcd524b8d759b42c7e
Matthew Kennedy <m.kennedy@me.com>
2022-05-25 23:17:04 +0100

    Clean up code.

--- app/javascript/spree/dashboard/controllers/ts/select_controller.js
+++ app/javascript/spree/dashboard/controllers/ts/select_controller.js
@@ -2,9 +2,5 @@ import StimulusTomSelect from './stimulus_tom_select'
 
 // Connects to data-controller="ts--select"
 export default class extends StimulusTomSelect {
-  initialize () {
-    this.config = {
-      plugins: []
-    }
-  }
+
 }
--- app/javascript/spree/dashboard/controllers/ts/stimulus_tom_select.js
+++ app/javascript/spree/dashboard/controllers/ts/stimulus_tom_select.js
@@ -22,11 +22,6 @@ class StimulusTomSelect extends Controller {
     if (this.hasOptionsValue) this.config.options = this.optionsValue
     if (this.hasPluginsValue) this.config.plugins = this.pluginsValue
 
-    // eslint-disable-next-line eqeqeq
-    if (this.element.options.length && this.element.options[0].value == '') {
-      if (!this.config.plugins.includes('clear_button')) this.config.plugins.push('clear_button')
-    }
-
     this.ts = new TomSelect(this.element, {
       ...this.config
     })
