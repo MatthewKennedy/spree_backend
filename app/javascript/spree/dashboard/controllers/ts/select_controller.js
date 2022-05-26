2b00b30e88908cc4fe10c540a1d461b70cef2cbe
Matthew Kennedy <m.kennedy@me.com>
2022-05-26 18:46:42 +0100

    fix JavaScript.

--- app/javascript/spree/dashboard/controllers/ts/select_controller.js
+++ app/javascript/spree/dashboard/controllers/ts/select_controller.js
@@ -2,5 +2,10 @@ import StimulusTomSelect from './stimulus_tom_select'
 
 // Connects to data-controller="ts--select"
 export default class extends StimulusTomSelect {
-
+  initialize () {
+    this.config = {
+      maxOptions: 500,
+      plugins: ['dropdown_input', 'no_backspace_delete']
+    }
+  }
 }
