17e5ece33e6b52915e83625553baee57de95f24c
Matthew Kennedy <m.kennedy@me.com>
2022-05-30 22:36:50 +0100

    Update

--- app/javascript/spree/dashboard/controllers/ts/templates/user_controller.js
+++ app/javascript/spree/dashboard/controllers/ts/templates/user_controller.js
@@ -2,9 +2,9 @@ import TsSearchController from '../search_controller.js'
 
 export default class extends TsSearchController {
   initialize () {
-    const valueField = this.valValue || 'id'
-    const labelField = this.txtValue || 'email'
-    const searchField = this.fieldsValue || ['email', 'first_name', 'last_name']
+    const valueField = 'id'
+    const labelField = 'email'
+    const searchField = ['email', 'first_name', 'last_name']
     const loadThrottle = this.loadThrottleValue || 400
 
     this.config = {
@@ -17,15 +17,27 @@ export default class extends TsSearchController {
       render: {
         option: this.render_option,
         item: this.render_item
-      }
+      },
+      onChange: (value) => this.doNext(value)
     }
   }
 
   render_option (data, escape) {
-    return `<div>${data.email}</div>`
+    console.log(data)
+    if (data.first_name && data.last_name) {
+      return `
+          <div>
+            <div>${escape(data.first_name)} ${escape(data.last_name)}</div>
+            <small class="text-muted">Email: ${escape(data.email)}</small>
+          </div>`
+    } else { return `<div>Email: ${escape(data.email)}</div>` }
   }
 
   render_item (data, escape) {
     return `<div>${data.email}</div>`
+  }
+
+  doNext (value) {
+    console.log(value)
   }
 }
