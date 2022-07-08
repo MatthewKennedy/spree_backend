0e6b541c12d68bbc1296edeff742ed8efc178269
Matthew Kennedy <m.kennedy@me.com>
2022-07-08 13:51:59 +0100

    Further slim down the javascript.

--- app/javascript/spree/dashboard/utilities/base.js
+++ app/javascript/spree/dashboard/utilities/base.js
@@ -1,36 +1,19 @@
 import Uri from 'jsuri'
 
 const SpreeDash = {}
-if (!window.SpreeDash) {
-  window.SpreeDash = SpreeDash
-}
 
-SpreeDash.mountedAt = function () {
-  return window.SpreePaths.mounted_at
-}
+if (!window.SpreeDash) { window.SpreeDash = SpreeDash }
 
-SpreeDash.adminPath = function () {
-  return window.SpreePaths.admin
-}
+SpreeDash.mountedAt = function () { return window.SpreePaths.mounted_at }
+SpreeDash.adminPath = function () { return window.SpreePaths.admin }
 
 SpreeDash.pathFor = function (path) {
-  const locationOrigin =
-    window.location.protocol +
-    '//' +
-    window.location.hostname +
-    (window.location.port ? ':' + window.location.port : '')
-
-  return this.url(
-    '' + locationOrigin + this.mountedAt() + path,
-    this.url_params
-  ).toString()
+  const locationOrigin = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '')
+  return this.url('' + locationOrigin + this.mountedAt() + path, this.url_params).toString()
 }
 
 SpreeDash.localizedPathFor = function (path) {
-  if (
-    typeof SPREE_LOCALE !== 'undefined' &&
-    typeof SPREE_CURRENCY !== 'undefined'
-  ) {
+  if (typeof SPREE_LOCALE !== 'undefined' && typeof SPREE_CURRENCY !== 'undefined') {
     const fullUrl = new URL(SpreeDash.pathFor(path))
     const params = fullUrl.searchParams
     let pathName = fullUrl.pathname
@@ -47,19 +30,9 @@ SpreeDash.localizedPathFor = function (path) {
   return SpreeDash.pathFor(path)
 }
 
-SpreeDash.adminPathFor = function (path) {
-  return this.pathFor('' + this.adminPath() + path)
-}
-
 SpreeDash.url = function (uri, query) {
-  if (uri.path === undefined) {
-    uri = new Uri(uri)
-  }
-  if (query) {
-    Object.keys(query).forEach(key => {
-      return uri.addQueryParam(key, query[key])
-    })
-  }
+  if (uri.path === undefined) { uri = new Uri(uri) }
+  if (query) { Object.keys(query).forEach(key => { return uri.addQueryParam(key, query[key]) }) }
   return uri
 }
 
