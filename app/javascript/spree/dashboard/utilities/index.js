d5a19f8c2ee5c8bd92e2a0ea457bad1f91a27c9b
Matthew Kennedy <m.kennedy@me.com>
2022-07-24 21:48:21 +0100

    Add custom confirm dialog

--- app/javascript/spree/dashboard/utilities/index.js
+++ app/javascript/spree/dashboard/utilities/index.js
@@ -1,5 +1,6 @@
 import './base'
 import './turbo_animations'
+import './custom_dialog_modal'
 import * as RequestUtility from './request_utility'
 
 if (!window.SpreeDash.RequestUtility) { window.SpreeDash.RequestUtility = RequestUtility }
