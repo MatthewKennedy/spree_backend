14aaef8ce026a770eb9f3328ba5959e08dc95ded
Matthew Kennedy <m.kennedy@me.com>
2022-08-07 21:19:55 +0100

    Move taxon product management into taxonomies.

--- app/javascript/spree/dashboard/vendors/ts_conditional_search.js
+++ app/javascript/spree/dashboard/vendors/ts_conditional_search.js
@@ -2,6 +2,6 @@ export default function (plugin_options) {
   this.hook('after', 'setup', function () {
     const optionsCount = Object.keys(this.options).length
 
-    if (optionsCount < 15) this.dropdown.classList.add('hideSearchInput')
+    if (optionsCount < 10) this.dropdown.classList.add('hideSearchInput')
   })
 }
